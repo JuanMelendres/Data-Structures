29c518634a10df1c2523b2fc4d3cf100db23a2f2
Juan Melendres <juanantoniomelendresvilla@MacBook-Air-de-Juan.local>
2017-02-08 12:37:59 -0600

    Adding new File Node.java

--- Node.java
+++ Node.java
@@ -21,10 +21,7 @@ public class Node<E>{
     public E getData(){
         return data;
     }
-    /**
-        * Obtiene el nodo que le sigue
-        * @return El nodo siguiente
-    */
+
     public Node getNext(){
         return this.next;
     }
@@ -41,27 +38,4 @@ public class Node<E>{
         return this.data + "";
     }
 
-    /*public static void main(String[] args) {
-        Node<Integer> n1 = new Node<Integer>(12);
-        Node<Integer> n2 = new Node<Integer>(2367);
-        Node<Integer> n3 = new Node<Integer>(0);
-        Node<Integer> n4 = new Node<Integer>(423675);
-
-        n1.setNext(n2);
-        n2.setNext(n3);
-        n3.setNext(n4);
-        n4.setNext(null);
-
-        System.out.println(n1.getData());
-        System.out.println(n1.getNext());
-        System.out.println();
-        System.out.println(n2.getData());
-        System.out.println(n2.getNext());
-        System.out.println();
-        System.out.println(n3.getData());
-        System.out.println(n3.getNext());
-        System.out.println();
-        System.out.println(n4.getData());
-        System.out.println(n4.getNext());
-    }*/
 }
