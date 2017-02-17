7b01bd8de4a18cb4d9b5e11a8574d06411aec0e6
Juan Melendres <juanantoniomelendresvilla@MacBook-Air-de-Juan.local>
2017-02-16 20:17:57 -0600

    Small changes

--- MergeSort/MergeSort2.java
+++ MergeSort/MergeSort2.java
@@ -1,3 +1,4 @@
+//Mergesort for ArrayList
 public class MergeSort2 {
 
     private ArrayListG<Integer> inputArray;
@@ -15,26 +16,22 @@ public class MergeSort2 {
     }
 
     public void divide(int startIndex,int endIndex){
-
         //Divide till you breakdown your list to single element
         if(startIndex<endIndex && (endIndex-startIndex)>=1){
             int mid = (endIndex + startIndex)/2;
             divide(startIndex, mid);
             divide(mid+1, endIndex);
-
             //merging Sorted array produce above into one sorted array
             merger(startIndex,mid,endIndex);
         }
     }
 
     public void merger(int startIndex,int midIndex,int endIndex){
-
-        //Below is the mergedarray that will be sorted array Array[i-midIndex] , Array[(midIndex+1)-endIndex]
+        //Below is the mergedarray that will be sorted array Array[i-midIndex]
+        //Array[(midIndex+1)-endIndex]
         ArrayListG<Integer> mergedSortedArray = new ArrayListG<Integer>();
-
         int leftIndex = startIndex;
         int rightIndex = midIndex+1;
-
         while(leftIndex<=midIndex && rightIndex<=endIndex){
             if(inputArray.get(leftIndex)<=inputArray.get(rightIndex)){
                 mergedSortedArray.add(inputArray.get(leftIndex));
@@ -44,18 +41,15 @@ public class MergeSort2 {
                 rightIndex++;
             }
         }
-
         //Either of below while loop will execute
         while(leftIndex<=midIndex){
             mergedSortedArray.add(inputArray.get(leftIndex));
             leftIndex++;
         }
-
         while(rightIndex<=endIndex){
             mergedSortedArray.add(inputArray.get(rightIndex));
             rightIndex++;
         }
-
         int i = 0;
         int j = startIndex;
         //Setting sorted array to original one
